6ed94ebf0a26e61e7213d59ab971732c59597fe8
Oskar <oskar0201@onet.pl>
2024-10-24 18:00:53 +0200

    feat(layout): integrate navigation and sidebar components with styling

--- university-pilot/app/layout.tsx
+++ university-pilot/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar/Sidebar";
 
 export const metadata: Metadata = {
   title: "University Pilot",
@@ -13,7 +16,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className="flex min-h-screen flex-col antialiased">
+        <div className="flex flex-1">
+          <Sidebar />
+          <div className="ml-24 flex flex-1 flex-col md:ml-48 lg:ml-72">
+            <Header />
+
+            <main className="ml-24 flex-1 p-6">{children}</main>
+
+            <Footer />
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
