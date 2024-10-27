cbb020103046a5f6339f6b5be9a949e6af8fa612
Michał Tracichleb <111907641+michal-tracichleb@users.noreply.github.com>
2024-10-27 09:33:45 +0100

    Add RepositoriesRegistration for DI setup and initialize in Program.cs

--- UniversityPilot/UniversityPilot/Program.cs
+++ UniversityPilot/UniversityPilot/Program.cs
@@ -16,6 +16,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddRepositories();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
