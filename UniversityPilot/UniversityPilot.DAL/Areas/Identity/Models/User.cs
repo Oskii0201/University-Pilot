ade9e3919c7144274016f8edb55ce6bc91832341
Michał Tracichleb <111907641+michal-tracichleb@users.noreply.github.com>
2024-10-27 09:25:35 +0100

    Update User model: replace Password with PasswordHash, add EmailIsConfirmed and optional PhoneNumber

--- UniversityPilot/UniversityPilot.DAL/Areas/Identity/Models/User.cs
+++ UniversityPilot/UniversityPilot.DAL/Areas/Identity/Models/User.cs
@@ -5,12 +5,15 @@ namespace UniversityPilot.DAL.Areas.Identity.Models
     public class User : IModelBase
     {
         public int Id { get; set; }
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public int RoleID { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
+        public bool EmailIsConfirmed { get; set; }
+        public string PasswordHash { get; set; }
+        public string? PhoneNumber { get; set; }
 
+        public int RoleID { get; set; }
         public virtual Role Role { get; set; }
     }
 }
--- UniversityPilot/UniversityPilot.DAL/UniversityPilotContext.cs
+++ UniversityPilot/UniversityPilot.DAL/UniversityPilotContext.cs
@@ -70,7 +70,13 @@ namespace UniversityPilot.DAL
                 entity.Property(e => e.FirstName).IsRequired().HasMaxLength(50);
                 entity.Property(e => e.LastName).IsRequired().HasMaxLength(50);
                 entity.Property(e => e.Email).IsRequired().HasMaxLength(100);
-                entity.Property(e => e.Password).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.EmailIsConfirmed)
+                      .HasDefaultValue(false)
+                      .IsRequired();
+                entity.Property(e => e.PasswordHash).IsRequired();
+                entity.Property(e => e.PhoneNumber)
+                      .HasMaxLength(24)
+                      .IsRequired(false);
                 entity.HasOne(e => e.Role)
                       .WithMany(r => r.Users)
                       .HasForeignKey(e => e.RoleID);
@@ -84,6 +90,9 @@ namespace UniversityPilot.DAL
             {
                 entity.HasKey(e => e.Id);
                 entity.Property(e => e.Name).IsRequired().HasMaxLength(50);
+                entity.HasMany(e => e.Users)
+                      .WithOne(u => u.Role)
+                      .HasForeignKey(u => u.RoleID);
             });
 
             #endregion Identity Configuration
