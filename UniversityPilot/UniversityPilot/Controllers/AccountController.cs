20e62732314327e2f8d8ac5f30afe5d470a7eeec
Michał Tracichleb <111907641+michal-tracichleb@users.noreply.github.com>
2024-10-28 18:31:11 +0100

    Create AccountController

---
+++ UniversityPilot/UniversityPilot/Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UniversityPilot.BLL.Areas.Identity.DTO;
+using UniversityPilot.BLL.Areas.Identity.Interfaces;
+
+namespace UniversityPilot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public IActionResult RegisterUser([FromBody] RegisterUserDto dto)
+        {
+            _accountService.RegisterUser(dto);
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult Login([FromBody] LoginDto dto)
+        {
+            string token = _accountService.GenerateJwt(dto);
+            return Ok(token);
+        }
+
+        [HttpGet]
+        [Route("Details")]
+        [Authorize]
+        public IActionResult Details(int id)
+        {
+            var userDetails = _accountService.GetUserDetails(id);
+            return Ok(userDetails);
+        }
+
+        [HttpPost]
+        [Route("Edit")]
+        [Authorize]
+        public IActionResult Edit(int id, [FromBody] UserDetailsDto userDetails)
+        {
+            _accountService.Edit(id, userDetails);
+            return Ok(userDetails);
+        }
+
+        [HttpPost]
+        [Route("ChangePassword")]
+        [Authorize]
+        public IActionResult ChangePassword(int id, [FromBody] ChangePasswordDto changePassword)
+        {
+            _accountService.ChangePassword(id, changePassword);
+            return Ok();
+        }
+    }
+}
