716cc3e470bfc7b23bab4b36e1269930ccb4dde0
Oskar <oskar0201@onet.pl>
2024-11-05 11:11:54 +0100

    style(footer): fix spaces and getting the year

--- university-pilot/components/Footer.tsx
+++ university-pilot/components/Footer.tsx
@@ -2,18 +2,18 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="bg-darkNavy text-offWhite py-4 text-center">
+    <footer className="bg-darkNavy py-4 text-center text-offWhite">
       <p>
-        &copy; 2024{" "}
+        &copy; {new Date().getFullYear()}{" "}
         <Link
           href="https://github.com/Oskii0201/University-Pilot"
           target="_blank"
           rel="noopener noreferrer"
           className="text-sky-500 hover:underline"
         >
-          University Pilot
-        </Link>
-        . All rights Reserved.
+          University Pilot.
+        </Link>{" "}
+        All rights Reserved.
       </p>
     </footer>
   );
