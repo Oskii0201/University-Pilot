1b0f038a635e5579bd327920fa72676b9212a67f
Oskar <oskar0201@onet.pl>
2024-11-05 12:48:47 +0100

    feat(dashboard): new dashboard main page

---
+++ university-pilot/app/dashboard/(overwiew)/page.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">
+        Witamy w aplikacji University Pilot
+      </h1>
+      <p className="mt-4">
+        Korzystaj z aplikacji do generowania siatek zajęć i zarządzania
+        zasobami!
+      </p>
+      <div className="rounded-lg bg-offWhite p-6 shadow-lg">
+        <h2 className="text-xl font-semibold text-darkGray">
+          Karta informacyjna
+        </h2>
+        <p className="text-darkGray">To jest przykładowy opis karty.</p>
+      </div>
+    </div>
+  );
+}
