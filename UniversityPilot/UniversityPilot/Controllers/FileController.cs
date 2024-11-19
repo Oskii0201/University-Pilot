dcabc03d0444ceefbe7d0b8dd6114bcb99b09b7f
Michał Tracichleb <111907641+michal-tracichleb@users.noreply.github.com>
2024-11-19 14:29:21 +0100

    Create endpoint for uploading CSV files

---
+++ UniversityPilot/UniversityPilot/Controllers/FileController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UniversityPilot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload(string dataset, IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("No file uploaded.");
+            }
+
+            if (Path.GetExtension(file.FileName).ToLower() != ".csv")
+            {
+                return BadRequest("The file is not in CSV format.");
+            }
+
+            // TODO: Przetwarzanie pliku CSV
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
+}
