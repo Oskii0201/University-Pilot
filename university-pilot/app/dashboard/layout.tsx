b7b4cf5e336db2ac178dfd0c180724031d590fe3
Oskar <oskar0201@onet.pl>
2024-11-19 14:04:01 +0100

    feat(Loading): add loadingCircle component to dashboard layout

--- university-pilot/app/dashboard/layout.tsx
+++ university-pilot/app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 import Sidebar from "@/components/Sidebar/Sidebar";
 import Header from "@/components/Header";
-import React from "react";
+import React, { Suspense } from "react";
+import Loading from "@/app/dashboard/loading";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -12,7 +13,9 @@ function Layout({ children }: LayoutProps) {
       <Sidebar />
       <div className="ml-24 flex flex-1 flex-col md:ml-48 lg:ml-72">
         <Header />
-        <main className="flex-1 p-6">{children}</main>
+        <Suspense fallback={<Loading />}>
+          <main className="flex-1 p-6">{children}</main>
+        </Suspense>
       </div>
     </div>
   );
---
+++ university-pilot/app/dashboard/loading.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { LoadingCircle } from "@/components/LoadingCircle";
+
+const Loading: React.FC = () => {
+  return <LoadingCircle />;
+};
+
+export default Loading;
