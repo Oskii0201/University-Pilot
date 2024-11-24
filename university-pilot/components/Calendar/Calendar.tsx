462bcce1eea381696b2a3ac3ce01343be0873675
Oskar <oskar0201@onet.pl>
2024-11-24 16:07:01 +0100

    feat(Calendar): add fetching calendarData

--- university-pilot/components/Calendar/Calendar.tsx
+++ university-pilot/components/Calendar/Calendar.tsx
@@ -1,15 +1,54 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { getCalendarDays } from "@/components/Calendar/lib/getCalendarDays";
+import { getWeekDays } from "@/components/Calendar/lib/getWeekDays";
+import MonthView from "@/components/Calendar/MonthView";
 import { CalendarHeader } from "./CalendarHeader";
+
+interface MonthData {
+  type: "month";
+  prevMonthDates: Date[];
+  currentMonthDates: Date[];
+  nextMonthDates: Date[];
+  rows: number;
+}
+
+interface WeekData {
+  type: "week";
+  weekDays: Date[];
+}
+
+type CalendarData = MonthData | WeekData;
 
 export const Calendar: React.FC = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
+  const [calendarData, setCalendarData] = useState<CalendarData | null>(null);
   const [view, setView] = useState<"month" | "week">("month");
+
+  useEffect(() => {
+    const fetchCalendarData = async () => {
+      if (view === "month") {
+        const calendarDays = getCalendarDays(currentDate);
+        setCalendarData({
+          type: "month",
+          ...calendarDays,
+        });
+      } else if (view === "week") {
+        const weekDays = getWeekDays(currentDate);
+        setCalendarData({
+          type: "week",
+          weekDays,
+        });
+      }
+    };
+    fetchCalendarData().catch(console.error);
+  }, [currentDate, view]);
+
   const handlePrevClick = () => {
     const newDate = new Date(currentDate);
     if (view === "month") {
       newDate.setMonth(currentDate.getMonth() - 1);
-    } else {
+    } else if (view === "week") {
       newDate.setDate(currentDate.getDate() - 7);
     }
     setCurrentDate(newDate);
@@ -19,15 +58,14 @@ export const Calendar: React.FC = () => {
     const newDate = new Date(currentDate);
     if (view === "month") {
       newDate.setMonth(currentDate.getMonth() + 1);
-    } else {
+    } else if (view === "week") {
       newDate.setDate(currentDate.getDate() + 7);
     }
     setCurrentDate(newDate);
   };
 
   const handleTodayClick = () => {
-    const newDate = new Date();
-    setCurrentDate(newDate);
+    setCurrentDate(new Date());
   };
 
   return (
@@ -40,7 +78,17 @@ export const Calendar: React.FC = () => {
         onViewChange={setView}
         currentView={view}
       />
-      {view === "month" ? <p className="">Month</p> : <p className="">Week</p>}
+
+      {view === "month" && calendarData?.type === "month" && (
+        <MonthView
+          prevMonthDates={calendarData.prevMonthDates}
+          currentMonthDates={calendarData.currentMonthDates}
+          nextMonthDates={calendarData.nextMonthDates}
+          rows={calendarData.rows}
+        />
+      )}
+
+      {view === "week" && calendarData?.type === "week" && <p>Week</p>}
     </div>
   );
 };
