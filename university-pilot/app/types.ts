55c529ece155390f8e1b341c3e2ac73e07a5984f
Oskar <oskar0201@onet.pl>
2024-12-16 17:37:04 +0100

    update types

--- university-pilot/app/types.ts
+++ university-pilot/app/types.ts
@@ -1,7 +1,8 @@
 export interface Event {
-  id: string; // Unikalny identyfikator
-  title: string; // Tytuł wydarzenia
-  description: string; // Opis wydarzenia
-  startTime: string; // Data i czas rozpoczęcia w formacie ISO
-  endTime: string; // Data i czas zakończenia w formacie ISO
+  id: string;
+  title: string;
+  description: string;
+  startTime: string;
+  endTime: string;
+  room?: string;
 }
