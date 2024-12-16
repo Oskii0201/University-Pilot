312b71e1719b77ef86c73bf1d5dc8b36c2531ff8
Oskar <oskar0201@onet.pl>
2024-12-16 17:17:16 +0100

    feat(calculateRange): add calculation for tableView

--- university-pilot/components/Calendar/lib/calculateRange.ts
+++ university-pilot/components/Calendar/lib/calculateRange.ts
@@ -1,13 +1,16 @@
 import { startOfMonth, endOfMonth, startOfWeek, endOfWeek } from "date-fns";
 
-/**
- * Oblicza zakres dat dla widoku miesięcznego lub tygodniowego,
- * uwzględniając tygodnie od poniedziałku do niedzieli.
- */
-export const calculateRange = (view: "month" | "week", date: Date) => {
+export const calculateRange = (
+  view: "month" | "week" | "table",
+  date: Date,
+) => {
   if (view === "month") {
-    const start = startOfWeek(startOfMonth(date), { weekStartsOn: 1 }); // Poniedziałek
-    const end = endOfWeek(endOfMonth(date), { weekStartsOn: 1 }); // Niedziela
+    const start = startOfWeek(startOfMonth(date), { weekStartsOn: 1 });
+    const end = endOfWeek(endOfMonth(date), { weekStartsOn: 1 });
+    return { start, end };
+  } else if (view === "table") {
+    const start = startOfMonth(date);
+    const end = endOfMonth(date);
     return { start, end };
   } else {
     const start = startOfWeek(date, { weekStartsOn: 1 });
