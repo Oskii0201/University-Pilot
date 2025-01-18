4272a524a84d6b66a3299b7613387e4d140b57c7
Michał Tracichleb <111907641+michal-tracichleb@users.noreply.github.com>
2025-01-18 15:51:01 +0100

    Add DTO classes: GroupCsv, HistoricalScheduleCsv, InstructorCsv, and StudyProgramCsv

---
+++ UniversityPilot/UniversityPilot.BLL/Areas/Files/DTO/InstructorCsv.cs
@@ -0,0 +1,19 @@
+﻿using UniversityPilot.BLL.Areas.Shared;
+
+namespace UniversityPilot.BLL.Areas.Files.DTO
+{
+    public class InstructorCsv
+    {
+        [CsvColumn(0, "ID")]
+        public int Id { get; set; }
+
+        [CsvColumn(1, "Tytuł")]
+        public string Title { get; set; }
+
+        [CsvColumn(2, "Imię")]
+        public string FirstName { get; set; }
+
+        [CsvColumn(3, "Nazwisko")]
+        public string LastName { get; set; }
+    }
+}
---
+++ UniversityPilot/UniversityPilot.BLL/Areas/Files/DTO/StudyProgramCsv.cs
@@ -0,0 +1,37 @@
+﻿using UniversityPilot.BLL.Areas.Shared;
+
+namespace UniversityPilot.BLL.Areas.Files.DTO
+{
+    public class StudyProgramCsv
+    {
+        [CsvColumn(0, "Rok immatrykulacji")]
+        public string EnrollmentYear { get; set; }
+
+        [CsvColumn(1, "Kierunek")]
+        public string FieldOfStudy { get; set; }
+
+        [CsvColumn(2, "Forma")]
+        public string StudyForm { get; set; }
+
+        [CsvColumn(3, "Specjalność")]
+        public string Specialization { get; set; }
+
+        [CsvColumn(4, "Nazwa przedmiotu")]
+        public string CourseName { get; set; }
+
+        [CsvColumn(5, "Semestr")]
+        public int SemesterNumber { get; set; }
+
+        [CsvColumn(6, "Rodzaj zajęć")]
+        public string CourseType { get; set; }
+
+        [CsvColumn(7, "Liczba godzin")]
+        public int Hours { get; set; }
+
+        [CsvColumn(8, "Forma zaliczenia")]
+        public string AssessmentType { get; set; }
+
+        [CsvColumn(9, "ECTS")]
+        public int ECTS { get; set; }
+    }
+}
