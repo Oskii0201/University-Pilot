4214db3af11701a73e5bbc6f2545710cdaae726c
Michał Tracichleb <111907641+michal-tracichleb@users.noreply.github.com>
2025-01-19 10:43:22 +0100

    Implement utility for enum description parsing

---
+++ UniversityPilot/UniversityPilot.DAL/Areas/Shared/Utilities/EnumHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace UniversityPilot.DAL.Areas.Shared.Utilities
+{
+    public static class EnumHelper
+    {
+        public static TEnum ParseEnumFromDescription<TEnum>(string description) where TEnum : Enum
+        {
+            foreach (var field in typeof(TEnum).GetFields(BindingFlags.Public | BindingFlags.Static))
+            {
+                var attribute = field.GetCustomAttribute<DescriptionAttribute>();
+                if (attribute != null && attribute.Description == description)
+                {
+                    return (TEnum)field.GetValue(null);
+                }
+            }
+
+            throw new ArgumentException($"No matching enum value found for description: {description}", nameof(description));
+        }
+
+        public static TEnum ParseEnumFromDescriptionOrDefault<TEnum>(string description, TEnum defaultValue) where TEnum : Enum
+        {
+            foreach (var field in typeof(TEnum).GetFields(BindingFlags.Public | BindingFlags.Static))
+            {
+                var attribute = field.GetCustomAttribute<DescriptionAttribute>();
+                if (attribute != null && attribute.Description == description)
+                {
+                    return (TEnum)field.GetValue(null);
+                }
+            }
+
+            return defaultValue;
+        }
+    }
+}
