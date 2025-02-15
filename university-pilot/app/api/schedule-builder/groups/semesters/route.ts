78a8297f07819d2f2a17b3268a1da0e24a3870e7
Oskar <oskar0201@onet.pl>
2025-02-15 18:35:26 +0100

    update endpoint to integrate with API

--- university-pilot/app/api/schedule-builder/groups/semesters/route.ts
+++ university-pilot/app/api/schedule-builder/groups/semesters/route.ts
@@ -1,21 +1,17 @@
 import { NextResponse } from "next/server";
-import { readFile } from "fs/promises";
+import apiClient from "@/app/lib/apiClient";
+import { handleApiError } from "@/utils/handleApiError";
 
 export async function GET() {
   try {
-    const semesters = await readFile("data/semesters.json", "utf-8");
-    return NextResponse.json(JSON.parse(semesters));
-
-    // Docelowy kod:
-    // const response = await apiClient.get("");
-    // if (!response.ok) throw new Error("Failed to fetch semesters");
-    // const data = await response.json();
-    // return NextResponse.json(data);
-  } catch (error) {
-    console.error("Error fetching semesters:", error);
-    return NextResponse.json(
-      { error: "Failed to fetch semesters" },
-      { status: 500 },
+    const result = await apiClient.get(
+      "/StudyProgram/GetUpcomingSemesters?count=-1",
     );
+
+    return NextResponse.json(result.data);
+  } catch (error) {
+    console.error(error);
+    const errorMessage = handleApiError(error);
+    return NextResponse.json({ error: errorMessage }, { status: 500 });
   }
 }
