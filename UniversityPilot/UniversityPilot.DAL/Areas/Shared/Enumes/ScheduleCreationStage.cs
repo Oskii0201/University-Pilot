bbf523afb7107ba06503bc0fa5153fbca425c1ff
Michał Tracichleb <michal.tracichleb@gmail.com>
2025-03-31 13:17:13 +0200

    Update ScheduleCreationStage enum and add .gitkeep folders for ML project

--- UniversityPilot/UniversityPilot.DAL/Areas/Shared/Enumes/ScheduleCreationStage.cs
+++ UniversityPilot/UniversityPilot.DAL/Areas/Shared/Enumes/ScheduleCreationStage.cs
@@ -2,10 +2,12 @@
 {
     public enum ScheduleCreationStage
     {
-        New = 0,
-        GroupsScheduleCreated = 1,
-        PreliminarySchedule = 2,
-        GeneratedSchedule = 3,
-        ApprovedSchedule = 4
+        New = 0, // Tworzenie grup pod określenie dni zjazdu
+        GroupsScheduleCreated = 1, // Określenie dni zjazdu
+        GeneratingPreliminarySchedule = 2, // Akceptacja zjazdów i przesłanie na tworzenie wstępnego hrmonogramu
+        PreliminarySchedule = 3, // Utworzenie wstępnego hrmonogramu
+        GeneratingSchedule = 4, // Tworzenie harmonogramu poprzez AI
+        GeneratedSchedule = 5, // Utworzony harmonogram AI
+        ApprovedSchedule = 6 // Zaakceptowany harmonogram
     }
 }
